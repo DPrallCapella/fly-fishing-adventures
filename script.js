d0c50f5ebfd66c38c50fb59257270fce7c8a33fc
DPrallCapella <dimply_grad.0m@icloud.com>
2024-11-25 15:00:48 -0700

    Assessment 4 V.1

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.querySelector('.hamburger-menu-button').addEventListener('click', () => {
     const menu = document.querySelector('.hamburger-menu');
     menu.classList.toggle('expanded');
-});
+})
+
+function openModal(imgElement) {
+    const modal = document.getElementById('imageModal');
+    const modalImg = document.getElementById('modalImage');
+    const caption = document.getElementById('caption');
+
+    modal.style.display = "block";
+    modalImg.src = imgElement.src;
+    caption.innerHTML = imgElement.alt;
+}
+
+function closeModal() {
+    const modal = document.getElementById('imageModal');
+    modal.style.display = "none";
+}
+
